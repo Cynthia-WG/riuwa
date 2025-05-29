3dd68025eaf93e9c864ec9eb35bc90736cf0b257
Cynthia-WG <cynthiagit316@gmail.com>
2025-05-29 11:42:24 +0300

    Added the function for date and time

--- index.js
+++ index.js
@@ -2,8 +2,37 @@ function displayWeather(response) {
   let temperatureElement = document.querySelector("#city-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let city = document.querySelector("#city-weather");
+  let descriptionELement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  console.log(response.data);
   city.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionELement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = `${Math.round(response.data.wind.speed)}`;
   temperatureElement.innerHTML = `${temperature}`;
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
 }
 function searchCity(city) {
   let apiKey = "36870e7a7a8911t30o6cf5301344fcb8";
